f02f93906cc0a391ae3bc8f22cc142f0a4798159
bissaliev <bissaliev21@gmail.com>
2024-08-14 12:30:22 +0400

    Добавление пользователя в чат

--- chat/consumers.py
+++ chat/consumers.py
@@ -1,12 +1,29 @@
 import json
 
+from channels.db import database_sync_to_async
 from channels.generic.websocket import AsyncWebsocketConsumer
+from django.contrib.auth import get_user_model
+from django.shortcuts import get_object_or_404
+
+from .models import Message, Room
+
+User = get_user_model()
 
 
 class ChatConsumer(AsyncWebsocketConsumer):
+    @database_sync_to_async
+    def create_message(self, message, username):
+        room = get_object_or_404(Room, room_name=self.room_name)
+        user = get_object_or_404(User, username=username)
+        new_msg = Message.objects.create(
+            sender=user, content=message, room=room
+        )
+        return new_msg
+
     async def connect(self):
         self.room_name = self.scope["url_route"]["kwargs"]["room_name"]
         self.room_group_name = f"chat_{self.room_name}"
+        self.user = self.scope["user"]
         await self.channel_layer.group_add(
             self.room_group_name, self.channel_name
         )
@@ -20,14 +37,27 @@ class ChatConsumer(AsyncWebsocketConsumer):
     async def receive(self, text_data=None, bytes_data=None):
         text_data_json = json.loads(text_data)
         message = text_data_json["message"]
+        username = text_data_json["username"]
+        new_msg = await self.create_message(message, username)
         await self.channel_layer.group_send(
             self.room_group_name,
             {
                 "type": "chat_message",
-                "message": message,
+                "message": new_msg.content,
+                "username": new_msg.sender.username,
+                "created_at": new_msg.created_at.isoformat(),
             },
         )
 
     async def chat_message(self, event):
         # отправить сообщение в веб-сокет
-        await self.send(text_data=json.dumps(event))
+        await self.send(
+            text_data=json.dumps(
+                {
+                    "type": "chat_message",
+                    "username": event["username"],
+                    "message": event["message"],
+                    "created_at": event["created_at"],
+                }
+            )
+        )
--- static/js/my_script.js
+++ static/js/my_script.js
@@ -1,26 +1,93 @@
+document.getElementById("chat").scrollTop = document.getElementById("chat").scrollHeight
 const roomName = JSON.parse(
     document.getElementById('room-name').textContent
 );
+const userName = JSON.parse(
+    document.getElementById('username').textContent
+);
 const url = 'ws://' + window.location.host + '/ws/chat/room/' + roomName + '/';
 const chatSocket = new WebSocket(url);
-chatSocket.onmessage = function (event) {
-    const data = JSON.parse(event.data);
+
+function timeAgo(date) {
+    const now = new Date();
+    const secondsPast = Math.floor((now - date) / 1000);
+
+    if (secondsPast < 60) {
+        return `${secondsPast} секунд назад`;
+    }
+    if (secondsPast < 3600) {
+        const minutes = Math.floor(secondsPast / 60);
+        return `${minutes} минут назад`;
+    }
+    if (secondsPast < 86400) {
+        const hours = Math.floor(secondsPast / 3600);
+        return `${hours} часов назад`;
+    }
+    if (secondsPast < 2592000) {
+        const days = Math.floor(secondsPast / 86400);
+        return `${days} дней назад`;
+    }
+    if (secondsPast < 31536000) {
+        const months = Math.floor(secondsPast / 2592000);
+        return `${months} месяцев назад`;
+    }
+    const years = Math.floor(secondsPast / 31536000);
+    return `${years} лет назад`;
+};
+
+// Получение шаблона для сообщения
+function getTemplate(userName, sender) {
+    if (userName == sender) {
+        return document.getElementById('message-template-me');
+    } else {
+        return document.getElementById('message-template');
+    }
+}
+
+function buildingMessageTemplate(data) {
+    const template = getTemplate(userName, data.username);
+    const messageElement = template.content.cloneNode(true);
+    messageElement.getElementById("message-content").textContent = data.message;
+    messageElement.getElementById("message-username").textContent = data.username;
+    const timestamp = new Date(data.created_at);
+    messageElement.getElementById("message-time").textContent = timeAgo(timestamp);
+    return messageElement
+};
+
+// Добавление шаблона сообщения в чат
+function addMessageToChat(data) {
     const chat = document.getElementById("chat");
-    chat.innerHTML += '<div class="d-flex flex-row justify-content-start">' + data.message +
-        '</div>';
+    messageElement = buildingMessageTemplate(data)
+    chat.appendChild(messageElement)
     chat.scrollTop = chat.scrollHeight;
 };
+
+// получение данных через веб-сокет
+chatSocket.onmessage = function (event) {
+    const data = JSON.parse(event.data);
+    addMessageToChat(data)
+};
+
+// Закрытие вебсокета
 chatSocket.onclose = function (event) {
     console.error('Chat socket closed unexpectedly');
 };
+
+// Отправка данных на сервер
+function sendMessage(message, userName) {
+    chatSocket.send(JSON.stringify({
+        'type': 'chat_message',
+        'message': message,
+        'username': userName
+    }));
+};
+
 const input = document.getElementById('chat-message-input');
 const submitButton = document.getElementById('chat-message-submit');
 submitButton.addEventListener('click', function (event) {
     const message = input.value;
     if (message) {
-        chatSocket.send(JSON.stringify({
-            'message': message
-        }));
+        sendMessage(message, userName)
         input.value = '';
         input.focus();
     }
