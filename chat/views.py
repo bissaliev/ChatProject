7c193deb2d2a49d01801dd58795b0546e0b08a2c
bissaliev <bissaliev21@gmail.com>
2024-08-14 12:28:59 +0400

    Добавление отображения сообщений из БД

--- chat/views.py
+++ chat/views.py
@@ -11,4 +11,13 @@ def index(request):
 def room(request, room_name):
     if not request.user.is_authenticated:
         return redirect("login")
-    return render(request, "chat/room.html", {"room_name": room_name})
+    current_room = Room.objects.get(room_name=room_name)
+    messages = current_room.messages.all().order_by("created_at")
+    return render(
+        request,
+        "chat/room.html",
+        {
+            "room_name": room_name,
+            "messages": messages,
+        },
+    )
